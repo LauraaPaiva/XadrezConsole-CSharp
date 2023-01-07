4097b82bd18bf44a179dcfc8c079d6ba1e2f3931
LauraaPaiva <lauraapaiva94@gmail.com>
2023-01-07 10:28:08 -0300

    Criação do tabuleiro

---
+++ xadrez_console/xadrez_console/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int quantidadeMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            quantidadeMovimentos= 0;
+        }
+    }
+}
