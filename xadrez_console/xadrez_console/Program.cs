9ed9612522609865fc59e9bade04b4f4326b2c90
LauraaPaiva <lauraapaiva94@gmail.com>
2023-01-07 10:39:54 -0300

    Método para imprimir o tábuleiro na tela

--- xadrez_console/xadrez_console/Program.cs
+++ xadrez_console/xadrez_console/Program.cs
@@ -1,5 +1,9 @@
 ﻿using tabuleiro;
+using xadrez_console;
 
 Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
+Tela.imprimirTabuleiro(tabuleiro);
+
 Console.ReadLine();
 
---
+++ xadrez_console/xadrez_console/Tela.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tabuleiro)
+        {
+            for (int i = 0; i < tabuleiro.linhas; i++)
+            {
+                for (int j = 0; j < tabuleiro.colunas; j++)
+                {
+                    if (tabuleiro.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tabuleiro.peca(i, j) + " ");
+                    }
+
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
